dfe85feb8afd43cf567e294cf1c97253c81b2543
yaozuopan <yaozuopan@weuseapp.com>
2021-06-15 15:47:56 +0800

    PDF

--- ImageCG/ICGImage.swift
+++ ImageCG/ICGImage.swift
@@ -45,6 +45,56 @@ extension ImageCG where Base: UIImage {
     public var color: UIColor? {
         UIColor(patternImage: base)
     }
+    
+    
+    /// 读取 PDF 并转为 UIImage
+    /// - Parameters:
+    ///   - filePath: PDF 路径
+    ///   - isJoin: 是否进行拼接，ture = 拼接成一张图片，false = 一页一张图片
+    ///   - color: 填充背景颜色
+    /// - Returns: 数组 [UIImage]
+    public static func readPDF(from filePath: String, isJoin: Bool = true, fill color: UIColor = .white) -> [UIImage?] {
+        let data = readPDF(from: filePath, fill: color)
+        guard isJoin else { return data.0 }
+        return [pdfJoin(from: data)]
+    }
+    
+    static func pdfJoin(from data: ([UIImage?], CGSize)) -> UIImage? {
+        return (drawImage(size: data.1, isOpaque: true) { (_) in
+            var offsetY: CGFloat = 0.0
+            for image in data.0 {
+                guard let image = image else { continue }
+                image.draw(in: CGRect(origin: .init(x: 0, y: offsetY), size: image.size))
+                offsetY += image.size.height
+            }
+        })
+    }
+    
+    static func readPDF(from filePath: String, fill color: UIColor = .white) -> ([UIImage?], CGSize) {
+        guard let pdf = CGPDFDocument(URL(fileURLWithPath: filePath) as CFURL) else { return ([], .zero) }
+        
+        var images: [UIImage?] = []
+        let pageRect: CGRect = pdf.page(at: 1)!.getBoxRect(.mediaBox)
+        
+        _ = drawImage(size: pageRect.size, isOpaque: true, { (context) in
+            for index in 1...pdf.numberOfPages {
+                context.saveGState()
+                color.setFill()
+                context.fill(CGRect(origin: .zero, size: pageRect.size))
+                
+                let pdfPage: CGPDFPage = pdf.page(at: index)!
+                context.translateBy(x: 0.0, y: pageRect.size.height)
+                context.scaleBy(x: 1.0, y: -1.0)
+                context.concatenate(pdfPage.getDrawingTransform(.mediaBox, rect: pageRect, rotate: 0, preserveAspectRatio: true))
+                context.drawPDFPage(pdfPage)
+                images.append(UIGraphicsGetImageFromCurrentImageContext())
+                context.clear(pageRect)
+                context.restoreGState()
+            }
+        })
+        
+        return (images, CGSize(width: pageRect.size.width, height: pageRect.size.height * CGFloat(pdf.numberOfPages)))
+    }
 }
 
 extension CGRect {
--- ImageCG/ImageCG.swift
+++ ImageCG/ImageCG.swift
@@ -20,22 +20,22 @@ public protocol ImageCGCompatible {
     associatedtype CompatibleType
     
     var icg: ImageCG<CompatibleType> { get }
+    static var icg: ImageCG<CompatibleType>.Type { get }
 }
 
 public extension ImageCGCompatible {
     
-    var icg: ImageCG<Self> {
-        get { return ImageCG(self) }
-    }
+    var icg: ImageCG<Self> { ImageCG(self) }
+    static var icg: ImageCG<Self>.Type {  ImageCG<Self>.self }
 }
 
 extension UIColor: ImageCGCompatible { }
 extension Array: ImageCGCompatible { }
 extension UIImage: ImageCGCompatible { }
 
-func drawImage(size: CGSize, _ draw: ((CGContext) -> Void)) -> UIImage? {
+func drawImage(size: CGSize, isOpaque: Bool = false, _ draw: ((CGContext) -> Void)) -> UIImage? {
     defer { UIGraphicsEndImageContext() }
-    UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
+    UIGraphicsBeginImageContextWithOptions(size, isOpaque, UIScreen.main.scale)
     guard let context  = UIGraphicsGetCurrentContext() else {
         return nil
     }
